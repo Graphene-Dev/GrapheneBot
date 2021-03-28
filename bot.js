1dd3799d06128b2f233cfdb5043375a46147f36b
GrantBGreat <GrantBGreat@users.noreply.github.com>
2021-03-28 14:40:10 -0400

    base files pushed

---
+++ bot.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const fs = require("fs");
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const TOKEN = process.env.TOKEN;
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err);
+  
+    let jsfile = files.filter(f => f.split(".").pop() === "js");
+    if(jsfile.length <= 0){
+      console.error("Couldn't find commands.");
+      return;
+    }
+  
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.info(`${f} loaded!`);
+    client.commands.set(props.help.name, props);
+  });
+  
+});
+
+client.on('ready', () => {
+  console.info(`\nLogged in as ${client.user.tag}!\n`);
+
+  client.user.setPresence({ activity: { name: 'Dear Alan Chu...' }, status: 'dnd' })
+    .then(console.log)
+    .catch(console.error);
+});
+
+client.on('message', message => {
+    // General checks:
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+    // Common vars
+    let content = message.content.split(" ");
+    let command = content[0].toLowerCase();
+    let args = content.slice(1);
+    let prefix = "-";
+
+    // Checks if message contains a command and runs it
+    let commandfile = client.commands.get(command.slice(prefix.length));
+    if(commandfile) commandfile.run(client,message,args);
+});
+
+client.login(TOKEN);
