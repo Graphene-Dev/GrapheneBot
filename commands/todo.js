c36dfabbb4410681b245d9fcc96114329d943050
Grant <grant@rbgn.ca>
2021-04-04 11:27:34 -0400

    db finally works thanks to grant the cool guy

--- commands/todo.js
+++ commands/todo.js
@@ -1,16 +1,14 @@
 const Discord = require('discord.js')
 var fs = require('fs')
 
+const { JsonDB } = require('node-json-db')
+const { Config } = require('node-json-db/dist/lib/JsonDBConfig')
+
 module.exports.run = async (client, message, args) => {
-    return // because code doesnt work
-    
-    // connect to the JSON db
-    const dir = './assets/todo.json'
+    var db = new JsonDB(new Config("todo", true, true, '/'));
+    var data = db.getData("/")
 
-    var data = fs.readFileSync(dir)
-    var jsondb = JSON.parse(data)
-
-    uid = message.author.id
+    var uid = message.author.id
 
     if (args[0] === 'add' || args[0] === '+') {
         if (!data.hasOwnProperty(uid)) {
@@ -26,6 +24,7 @@ module.exports.run = async (client, message, args) => {
         if (!data.hasOwnProperty(uid)) {
             return message.reply("you have no active tasks.")
         }
+
     }
 
     return
