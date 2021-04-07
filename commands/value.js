d104d2e01884a1caaf5e39c1362a574129b5e360
Grant Bourne <72450527+GrantBGreat@users.noreply.github.com>
2021-04-07 10:29:11 -0400

    Update value.js

--- commands/value.js
+++ commands/value.js
@@ -2,7 +2,13 @@ const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
     // gonna have to work on this command when there is actual value lol
-    return message.reply("our value is currently $0.00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.");
+    let val = getVal().toString();
+    return message.reply(`our value is currently $${val}.`);
+}
+
+function getVal() {
+    // get the value from the api
+    return 0
 }
 
 //The command's name
