412224b89302113b2ecdcd82555d5f9d7964d198
Grant <grant@rbgn.ca>
2021-04-08 09:26:47 -0400

    smore base code

--- commands/warn.js
+++ commands/warn.js
@@ -1,7 +1,15 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, message, args) => {
+    var messageResponse = '';
+    const user2warn = message.mentions.users.first();
+    if (user2warn === undefined) return message.reply("mention a user, using the syntax `$ban <user> [reason]`\nie: `$warn StealthHydrac#8476 being a poopoo`");
+
     return message.reply('no u.');
+}
+
+function warnMember(member, level) {
+
 }
 
 //The command's name
