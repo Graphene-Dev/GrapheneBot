5a5c342d9ff39e113c2bbba1c65c7d589bcf4294
Grant <grant@rbgn.ca>
2021-04-08 08:34:49 -0400

    added base stuff for warn command

---
+++ commands/warn.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (client, message, args) => {
+    return;
+}
+
+//The command's name
+module.exports.help = {
+  name: "warn",
+  description: "Very totally normal warn command using levels & stuffs."
+}
